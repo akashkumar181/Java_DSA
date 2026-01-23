8e87f7f55258970d6cef19d82b437c43abdd9acc
Akash Kumar <akash805198@gmail.com>
2026-01-23 11:24:15 +0530

    Pattern

--- Class/Pattern.java
+++ Class/Pattern.java
@@ -1,17 +1,187 @@
 package Class;
+import java.util.Scanner;
 
 public class Pattern {
     public static void main(String[] args){
-        System.out.println("Hello world");
-        int x=45;
-        int y=25;
-        System.out.println(x-y);
-        // Byte example
-        byte byteValue1 = 10;
-        byte byteValue2 = 20;
-        byte byteSum = (byte) (byteValue1 + byteValue2);
-        System.out.println("Byte Sum: " + byteSum);
-        System.out.println("Byte Min Value: " + Byte.MIN_VALUE);
-        System.out.println("Byte Max Value: " + Byte.MAX_VALUE);
+       //normal pattern
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter you row no: ");
+       int n=sc.nextInt();
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            System.out.print(" * ");
+        }
+        System.out.println();
+       }
+        System.out.println();
+    //next--
+       for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print(" * ");
+        }
+        System.out.println();
+       }
+        System.out.println();
+    //next--
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            System.out.print(" ");
+        }
+        for(int k=0;k<i+1;k++){
+            System.out.print("*");
+        }
+        System.err.println();
+    }
+     System.out.println();
+    // //next-- pascal Triangle
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            System.out.print(" ");
+        }
+        for(int k=0;k<2*i+1;k++){
+            System.out.print("*");
+        }
+        for(int m=0;m<n-i-1;m++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+    //next-- frame
+   System.out.println();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j==0 || j==n-1 || i==0|| i==n-1 ){
+                System.out.print("* ");
+            }else{
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+    }
+     System.out.println();
+    //next-- Diamond shape
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            System.out.print(" ");
+        }
+        for(int k=0;k<2*i+1;k++){
+            System.out.print("*");
+        }
+        for(int m=0;m<n-i-1;m++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+     for(int i=n-2;i>=0;i--){
+        for(int j=0;j<n-i-1;j++){
+            System.out.print(" ");
+        }
+        for(int k=0;k<2*i+1;k++){
+            System.out.print("*");
+        }
+        for(int m=0;m<n-i-1;m++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+   System.out.println();
+    
+      for(int i=n-1;i>=0;i--){
+        for(int j=0;j<n-i-1;j++){
+            System.out.print(" ");
+        }
+        for(int k=0;k<2*i+1;k++){
+            System.out.print("*");
+        }
+        for(int m=0;m<n-i-1;m++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+    //next--
+     System.out.println();
+     for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print(j+1);
+        }
+        for(int k=0;k<2*n-2-2*i;k++){
+            System.out.print(" ");
+        }
+        for(int p=i+1;p>=1;p--){
+            
+            System.out.print(p);
+           
+        }
+        System.out.println();
+     }
+     //next--
+      System.out.println();
+     for(int i=n-1;i>=0;i--){
+        for(int j=0;j<i+1;j++){
+            System.out.print("*");
+        }
+        for(int k=0;k<2*n-2-2*i;k++){
+            System.out.print(" ");
+        }
+        for(int p=i+1;p>=1;p--){
+            
+            System.out.print("*");
+           
+        }
+        System.out.println();
+     }
+      
+     for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print("*");
+        }
+        for(int k=0;k<2*n-2-2*i;k++){
+            System.out.print(" ");
+        }
+        for(int p=i+1;p>=1;p--){
+            
+            System.out.print("*");
+           
+        }
+        System.out.println();
+     }
+      System.out.println();
+      //next--
+      for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print("*");
+        }
+        for(int k=0;k<n-i-3;k++){
+            System.out.print(" ");
+        }
+         System.out.println();
+      }
+      for(int i=n-2;i>=0;i--){
+        for(int j=0;j<i+1;j++){
+            System.out.print("*");
+        }
+        for(int k=0;k<n-i-3;k++){
+            System.out.print(" ");
+        }
+         System.out.println();
+      }
+      //next--
+      for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            if((i+j)%2==0){
+                System.out.print(1);
+            }else{
+                System.out.print(0);
+            }
+         
+        }
+         System.out.println();
+      }
+
+
+    
+   
+
+
     }
 }
